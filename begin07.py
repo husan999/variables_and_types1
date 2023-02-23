0db02f8a2b84259a30c25d9c7dde8532a40fa9bb
husan999 <husanshunqorov07@gmail.com>
2023-02-22 20:49:26 -0700

    good

--- begin07.py
+++ begin07.py
@@ -5,3 +5,8 @@
 # Assign 8 (int value) to 'c' variable;
 
 # Print the result of that expression 'https://github.com/codeschool-2022A/variables_and_types#begin07';
+a=5
+b=4
+c=8
+k=(2*a/b)*c
+print(k)
